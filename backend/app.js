1f238e82118029ce447cb2e9ff102b63f915a460
Ajju-Chauhan <142608291+Ajju-Chauhan@users.noreply.github.com>
2025-02-21 16:40:28 +0530

    push all code

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,5 @@
+require("dotenv").config(); // Load environment variables
+
 const express = require("express");
 const mongoose = require("mongoose");
 const doctorRoutes = require("./routes/doctorRoutes");
@@ -6,17 +8,26 @@ const cors = require("cors");
 
 const app = express();
 
-
 // Allow CORS for all origins
 app.use(cors());
-
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
+const MONGO_URI = process.env.MONGODB_URI; // Ensure this is set in environment variables
+
+if (!MONGO_URI) {
+  console.error("Error: Missing MONGO_URI. Set it in environment variables.");
+  process.exit(1); // Exit the application if URI is missing
+}
+
+mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
+  });
 
 // Routes
 app.use("/api/doctors", doctorRoutes);
@@ -27,6 +38,3 @@ const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
